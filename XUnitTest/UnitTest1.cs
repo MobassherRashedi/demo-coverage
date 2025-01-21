4485b06da29e2cfae36652b5af6bd3dd234893ce
MD Mobassher Rashedi <cse.rashedi@gmail.com>
2025-01-22 02:30:57 +0600

    initial-implimentation

---
+++ XUnitTest/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using CoverX.Api.Services;
+using Xunit;
+
+namespace CoverX.Api.Tests
+{
+    public class CalculatorServiceTests
+    {
+        private CalculatorService _calculator;
+
+        public CalculatorServiceTests()
+        {
+            _calculator = new CalculatorService();
+        }
+
+        [Fact]
+        public void Add_ShouldReturnCorrectSum()
+        {
+            // Arrange 
+            int a = 10;
+            int b = 2;
+
+            // Act
+            var result = _calculator.Add(a, b);
+
+            // Assert
+            Assert.Equal(12, result);
+        }
+
+        [Fact]
+        public void Substract_ShouldReturnCorrectSum()
+        {
+            // Arrange 
+            int a = 10;
+            int b = 2;
+
+            // Act
+            var result = _calculator.Substract(a, b);
+
+            // Assert
+            Assert.Equal(8, result);
+        }
+    }
+}
---
+++ demo-coverage-test/Services/CalculatorService.cs
@@ -0,0 +1,10 @@
+﻿namespace CoverX.Api.Services
+{
+    public class CalculatorService : ICalculatorService
+    {
+        public int Add(int a, int b) => a + b;
+        public int Substract(int a, int b) => a - b;
+        public int Multiplication(int a, int b) => a * b;
+        public int Division(int a, int b) => a / b;
+    }
+}
